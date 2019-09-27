8fbd8042b81393e54cf3b0d50ab76c16e68c9236
Harald Ringvold <harald.ringvold@gmail.com>
2019-09-27 02:06:37 +0200

    Store tracking as duration on day

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,13 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
+import DateFormat
+import Dict exposing (Dict)
+import Duration exposing (Duration)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Quantity
 import Task
 import Time exposing (Posix, Zone)
 
@@ -15,6 +19,18 @@ import Time exposing (Posix, Zone)
 type alias Model =
     { tracking : Tracking
     , timeZone : Zone
+    , currentTime : Maybe Posix
+    , dates : Dates
+    }
+
+
+type alias Dates =
+    Dict String Day
+
+
+type alias Day =
+    { date : Posix
+    , duration : Duration
     }
 
 
@@ -36,8 +52,10 @@ init : ( Model, Cmd Msg )
 init =
     ( { tracking = NotStarted
       , timeZone = Time.utc
+      , currentTime = Nothing
+      , dates = Dict.empty
       }
-    , Cmd.batch [ Task.perform TimeZoneReceived Time.here ]
+    , Cmd.batch [ Task.perform TimeZoneReceived Time.here, Task.perform CurrentTimeReceived Time.now ]
     )
 
 
@@ -49,7 +67,8 @@ type Msg
     = StartTimeClicked
     | StopTimeClicked StartTime
     | ContinueTimeClicked StartTime
-    | CurrentTimeReceived Tracking
+    | UpdatedTrackingReceived Tracking
+    | CurrentTimeReceived Posix
     | TimeZoneReceived Zone
 
 
@@ -57,19 +76,66 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         StartTimeClicked ->
-            ( model, Cmd.batch [ Task.perform (CurrentTimeReceived << Started) Time.now ] )
+            ( model, Cmd.batch [ Task.perform (UpdatedTrackingReceived << Started) Time.now ] )
 
         StopTimeClicked startTime ->
-            ( model, Cmd.batch [ Task.perform (CurrentTimeReceived << Ended startTime) Time.now ] )
+            ( model, Cmd.batch [ Task.perform (UpdatedTrackingReceived << Ended startTime) Time.now ] )
 
         ContinueTimeClicked startTime ->
             ( { model | tracking = Started startTime }, Cmd.none )
 
-        CurrentTimeReceived tracking ->
-            ( { model | tracking = tracking }, Cmd.none )
+        UpdatedTrackingReceived tracking ->
+            case tracking of
+                Started time ->
+                    ( { model | tracking = tracking }, Cmd.none )
+
+                Ended start stop ->
+                    let
+                        date =
+                            dateString model.timeZone start
+
+                        duration =
+                            Duration.from start stop
+
+                        newDates =
+                            updateTrackings date start model.dates duration
+                    in
+                    ( { model | tracking = tracking, dates = newDates }, Cmd.none )
+
+                NotStarted ->
+                    ( model, Cmd.none )
+
+        CurrentTimeReceived ct ->
+            ( { model | currentTime = Just ct }, Cmd.none )
 
         TimeZoneReceived zone ->
             ( { model | timeZone = zone }, Cmd.none )
+
+
+updateTrackings : String -> Posix -> Dates -> Duration -> Dates
+updateTrackings key today dates newDuration =
+    Dict.update key
+        (\value ->
+            case value of
+                Just ({ duration } as day) ->
+                    Just
+                        { day
+                            | duration = Quantity.plus duration newDuration
+                        }
+
+                Nothing ->
+                    Just { date = today, duration = newDuration }
+        )
+        dates
+
+
+dateString : Zone -> Posix -> String
+dateString =
+    DateFormat.format
+        [ DateFormat.dayOfMonthFixed
+        , DateFormat.monthFixed
+        , DateFormat.yearNumber
+        ]
 
 
 
@@ -81,6 +147,7 @@ view model =
     div []
         [ h1 [] [ text "Time tracking" ]
         , viewTracking model
+        , viewDates model
         ]
 
 
@@ -104,15 +171,43 @@ viewTracking model =
                 ]
 
 
+viewDates : Model -> Html msg
+viewDates model =
+    Dict.toList model.dates
+        |> List.map (viewDate model.timeZone)
+        |> div []
+
+
+viewDate : Zone -> ( String, Day ) -> Html msg
+viewDate zone ( key, day ) =
+    div [] [ h2 [] [ text <| displayDate zone day.date ++ " : " ++ showDuration day.duration ] ]
+
+
+displayDate : Zone -> Posix -> String
+displayDate =
+    DateFormat.format
+        [ DateFormat.dayOfMonthFixed
+        , DateFormat.text "."
+        , DateFormat.text " "
+        , DateFormat.monthNameAbbreviated
+        ]
+
+
+showDuration duration =
+    if duration |> Quantity.lessThan (Duration.minutes 60) then
+        String.concat [ String.fromInt <| round <| Duration.inMinutes duration, " minutes" ]
+
+    else
+        String.concat [ String.fromInt <| round <| Duration.inHours duration, " hours" ]
+
+
 hourAndMinute : Zone -> Posix -> String
-hourAndMinute zone startTime =
-    let
-        toString transformer =
-            transformer zone startTime
-                |> String.fromInt
-                |> String.padLeft 2 '0'
-    in
-    toString Time.toHour ++ ":" ++ toString Time.toMinute
+hourAndMinute =
+    DateFormat.format
+        [ DateFormat.hourMilitaryFixed
+        , DateFormat.text ":"
+        , DateFormat.minuteFixed
+        ]
 
 
 
